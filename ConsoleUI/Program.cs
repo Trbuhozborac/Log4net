392c3967e3ac813ead51777933ef3c60654ef197
pomorange <nemanjapipi@hotmail.com>
2019-06-28 19:50:42 +0200

    added and tested basic log4net

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
 namespace ConsoleUI
 {
     class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Pokrenuo si Log4Net");
+
+            log.Error("This is my error message");
+
+            Console.ReadLine();
         }
     }
 }
